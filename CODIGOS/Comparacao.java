555baf11766d9112a7e435ea628c50ac82b6dc7f
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2022-06-13 21:22:07 -0300

    BUBBLE/SELECTION/INSERTION/QUICK/MERGE/ARRAYS

--- CODIGOS/Comparacao.java
+++ CODIGOS/Comparacao.java
@@ -9,18 +9,23 @@ public class Comparacao {
         BubbleSort bs = new BubbleSort();
         SelectionSort ss = new SelectionSort();
         InsertionSort is = new InsertionSort();
+        QuickSort qs = new QuickSort();
+        MergeSort ms = new MergeSort();
 
-        int arr[] = new int[10000];
+        int arr[] = new int[1000];
         int valor = arr.length-1;
         for (int i = 0; i < arr.length; i++) {
-            //arr[i] = i;
-            // arr[i] = valor--;
-            arr[i] = (int)(Math.random()*5000);
+               // arr[i] = i;
+                // arr[i] = valor--;
+                 //arr[i] = (int)(Math.random()*5000);
         }
         Instant comeca = Instant.now();
-             //bs.bubbleSort(arr);
-            //ss.SelectionSort(arr);
-           // is.InsertionSort(arr);
+             // bs.bubbleSort(arr);
+             //  ss.SelectionSort(arr);
+             //is.InsertionSort(arr);
+        // qs.qSort(arr, 0, arr.length - 1);
+        // ms.mSort(arr, 0, arr.length);
+         aSort(arr);
         Instant termina = Instant.now();
         Duration tempo = Duration.between(comeca,termina);
         imprime(arr);
@@ -28,5 +33,9 @@ public class Comparacao {
     }
     static void imprime(int[] lista){
         System.out.println(Arrays.toString(lista));
+    }
+
+    static void aSort(int[] a){
+      Arrays.sort(a);  
     }
 }
